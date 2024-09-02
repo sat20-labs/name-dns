6d45cfde1fc3985badb73e6259211d81fbfd206f
softwarecheng <softwarecheng@126.com>
2024-09-02 16:26:55 +0800

    fix

---
+++ server/ns/handler-sitemap.go
@@ -0,0 +1,7 @@
+package ns
+
+import "github.com/gin-gonic/gin"
+
+func (s *Service) siteMap(c *gin.Context) {
+
+}
--- server/ns/router.go
+++ server/ns/router.go
@@ -1,11 +1,17 @@
 package ns
 
 import (
+	"embed"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	common "github.com/sat20-labs/name-dns/common"
 	serverCommon "github.com/sat20-labs/name-dns/server/define"
 	"go.etcd.io/bbolt"
 )
+
+//go:embed static/*
+var staticFiles embed.FS
 
 type Service struct {
 	RpcConfig     *serverCommon.Rpc
@@ -35,9 +41,16 @@ func (s *Service) Init(r *gin.Engine) (err error) {
 }
 
 func (s *Service) initRouter(r *gin.Engine) {
+	staticServer := http.FS(staticFiles)
+	r.StaticFS("/static", staticServer)
+	r.GET("/favicon.ico", func(c *gin.Context) {
+		c.FileFromFS("static/favicon.ico", staticServer)
+	})
+	r.GET("/sitemap.xml", s.siteMap)
+	r.GET("/robots.txt", func(c *gin.Context) {
+		c.FileFromFS("static/robots.txt", staticServer)
+	})
+
 	r.GET("/", s.content)
 	r.GET("/summary/name-count", s.countHtml)
-	// r.GET("/content", s.content)
-	// r.GET("/content", s.content)
-	// g := r.Group("/")
 }
